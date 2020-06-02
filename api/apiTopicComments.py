38fe755e630b81d992ad1b59e0a5f01b110225d5
David Bellers <davidbellers@comentec.com>
2020-06-02 13:18:34 -0400

    Topic and TopicComment api routes

--- api/apiTopicComments.py
+++ api/apiTopicComments.py
@@ -1,46 +1,42 @@
-from flask import Blueprint, jsonify, abort
-from database.models import Project, Topic, Member
+from flask import Blueprint, jsonify, abort, request
+from database.models import Project, Topic, Member, TopicComment
 from api.localpayload import payload
+from sqlalchemy.orm import lazyload
 
-members_api = Blueprint('members_api', __name__)
+topiccomments_api = Blueprint('topiccomments_api', __name__)
 
+@topiccomments_api.route('/projects/<project_id>/topics/<topic_id>/comments',methods=['POST'])
+def create_comment(project_id,topic_id):
 
-@members_api.route("/members",methods=['GET'])
-def get_members():
-    
-  return jsonify({
-        'success':True,
-        'route':'Get all'
-      })
-    
-@members_api.route('/members/<member_id>',methods=['GET'])
-def get_member(project_id):
+  data = request.get_json()
+  comment_data = data.get('comment')
 
-  return jsonify({
-        'success':True,
-        'route':'Get single'
+  if not comment_data:
+        return jsonify({
+        'success':False,
+        'message':'No comment data provided'
       })
 
-@members_api.route('/members',methods=['POST'])
-def create_member():
+  topic = Topic.query.get(topic_id)
+  if not topic:
+        abort(403)
+
+  member = Member.query.options(lazyload(Member.projects)).filter(
+        Member.auth0_id == payload.get('sub', '')).one_or_none()
+  
+  if not member:
+        abort(403)
+
+  if not project_id in [ p.id for p in member.projects ]:
+        abort(403)
+
+  comment_data.topic_id = topic_id
+  comment_data.member_id = member.id
+
+  comment = TopicComment(**comment_data)
+  comment.insert()
 
   return jsonify({
         'success':True,
-        'route':'Create'
-      })
-
-@members_api.route('/members/<member_id>',methods=['PATCH'])
-def update_member(project_id):
-
-  return jsonify({
-        'success':True,
-        'route':'Update'
-      })
-
-@members_api.route('/members/<member_id>',methods=['DELETE'])
-def delete_member(project_id):
-
-  return jsonify({
-        'success':True,
-        'route':'Delete'
+        'comment':comment
       })
--- api/apiTopics.py
+++ api/apiTopics.py
@@ -1,46 +1,104 @@
-from flask import Blueprint, jsonify, abort
+from flask import Blueprint, jsonify, abort, request
 from database.models import Project, Topic, Member
 from api.localpayload import payload
+from sqlalchemy.orm import lazyload
 
-members_api = Blueprint('members_api', __name__)
-
-
-@members_api.route("/members",methods=['GET'])
-def get_members():
+topics_api = Blueprint('topics_api', __name__)
     
-  return jsonify({
-        'success':True,
-        'route':'Get all'
-      })
-    
-@members_api.route('/members/<member_id>',methods=['GET'])
-def get_member(project_id):
+@topics_api.route('/projects/<project_id>/topics/<topic_id>',methods=['GET'])
+def get_topic(project_id,topic_id):
+
+  member = Member.query.filter(
+        Member.auth0_id == payload.get('sub', '')).one_or_none()
+  
+  if not member:
+        abort(403)
+  
+  project = Project.query.options(lazyload(Project.members)).get(project_id)
+  if not project:
+        abort(404)
+  
+  if not member.id in [ m.id for m in project.members ]:
+        abort(403)
+
+  topic = Topic.query.options(lazyload(Topic.comments)).get(topic_id)
 
   return jsonify({
         'success':True,
-        'route':'Get single'
-      })
-
-@members_api.route('/members',methods=['POST'])
-def create_member():
-
-  return jsonify({
-        'success':True,
-        'route':'Create'
-      })
-
-@members_api.route('/members/<member_id>',methods=['PATCH'])
-def update_member(project_id):
-
-  return jsonify({
-        'success':True,
-        'route':'Update'
+        'topic':topic.long()
       })
 
-@members_api.route('/members/<member_id>',methods=['DELETE'])
-def delete_member(project_id):
+@topics_api.route('/projects/<project_id>/topics',methods=['POST'])
+def create_topic(project_id):
+
+  data = request.get_json()
+  topic_data = data.get('topic')
+
+  if not topic_data:
+        abort(405)
+
+  member = Member.query.filter(
+        Member.auth0_id == payload.get('sub', '')).one_or_none()
+  
+  if not member:
+        abort(403)
+  
+  project = Project.query.options(lazyload(Project.members)).get(project_id)
+  if not project:
+        abort(404)
+  
+  if not member.id in [ m.id for m in project.members ]:
+        abort(403)
+
+  topic_data['project_id'] = project.id
+  topic_data['member_id'] = member.id
+
+  topic = Topic(**topic_data)
+  topic.insert()
 
   return jsonify({
         'success':True,
-        'route':'Delete'
+        'topic':topic.long()
+      })
+
+@topics_api.route('/topics/<topic_id>',methods=['PATCH'])
+def update_topic_visibility(topic_id):
+  #This only updates the visibility for now
+
+  data = request.get_json()
+  topic_data = data.get('topic')
+
+  topic = Topic.query.get(topic_id)
+
+  member = Member.query.filter(
+        Member.auth0_id == payload.get('sub', '')).one_or_none()
+
+  if not member.id == topic.member_id:
+        abort(403)
+
+  topic.visibility = topic_data.get('visibility','CLOSED')
+
+  topic.update()
+
+  return jsonify({
+        'success':True,
+        'topic':topic.long()
+      })
+
+@topics_api.route('/topics/<topic_id>',methods=['DELETE'])
+def delete_topic(topic_id):
+
+  topic = Topic.query.get(topic_id)
+
+  member = Member.query.filter(
+        Member.auth0_id == payload.get('sub', '')).one_or_none()
+
+  if not member.id == topic.member_id:
+        abort(403)
+
+  topic.delete()
+
+  return jsonify({
+        'success':True,
+        'topic_id':topic_id
       })
