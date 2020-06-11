ab6838ac775cf86ac0d82041b6435c7d395de560
David Bellers <davidbellers@comentec.com>
2020-06-10 20:00:37 -0400

    Auth fix

--- auth/auth.py
+++ auth/auth.py
@@ -124,17 +124,12 @@ def verify_decode_jwt(token):
 
     if rsa_key:
         try:
-            '''payload = jwt.decode(
+            payload = jwt.decode(
                 token,
                 rsa_key,
                 algorithms=ALGORITHMS,
                 audience=API_AUDIENCE,
                 issuer='https://' + AUTH0_DOMAIN + '/'
-            )'''
-            payload = jwt.decode(
-                token,
-                rsa_key,
-                algorithms=ALGORITHMS
             )
 
             return payload
